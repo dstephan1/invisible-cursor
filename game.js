208609a4e8a3b3aae53326523773ba564cf70074
David Stephan <stephandavid3@gmail.com>
2017-07-22 11:29:23 -0700

    separated html and js, added parameter box

---
+++ game.js
@@ -0,0 +1,328 @@
+
+GameState = {
+	UNINITIALIZED: 0,
+	START : 1,
+	PLAY : 2,
+	FINISH : 3
+}
+
+var center;
+var center_textbox;
+var stats_textbox;
+var stats_textbox2;
+var canvas;
+var ctx;
+var width, height;
+
+var state = GameState.UNINITIALIZED;
+var clicks = 0;
+var time_elapsed_sec = 0;
+
+var last_click_sec = 0;
+var last_click_x = 0;
+var last_click_y = 0;
+
+var timeout_sec;
+var prev_t_msec; // used for calculating dt, microseconds
+
+var music;
+var player;
+
+Target = {
+	"x" : 0,
+	"y" : 0, 
+	"radius" : 40,
+	"fadein": 0 // TODO
+}
+var targets = [];
+
+// parameters
+// var circle_radius_min = 50;
+// var circle_radius_max = 100;
+// var num_targets = 3;
+// var spawn_width = 600;
+// var spawn_height = 500;
+// var hint_radius = 10;
+
+parameters = {
+	'circle_radius_min' : {
+		'min' : 1,
+		'max' : 500,
+		'default' : 50
+	},
+	'circle_radius_max' : {
+		'min' : 1,
+		'max' : 500,
+		'default' : 100
+	},
+	'num_targets' : {
+		'min' : 1,
+		'max' : 40,
+		'default' : 3
+	},
+	'spawn_width' : {
+		'min' : 0,
+		'max' : 800,
+		'default' : 600
+	},
+	'spawn_height' : {
+		'min' : 0,
+		'max' : 600,
+		'default' : 500
+	},
+	'hint_radius' : {
+		'min' : 0,
+		'max' : 500,
+		'default' : 10
+	}
+}
+var params; // gets loaded later
+
+
+window.onload = function() {
+	console.log("おはいよう");
+	// copy paste google's youtube api tutorial
+	var tag = document.createElement('script');
+
+	tag.src = "https://www.youtube.com/iframe_api";
+	var firstScriptTag = document.getElementsByTagName('script')[0];
+	firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+	// text stuff
+	center = document.getElementById("canvas-container");
+	center_textbox = document.getElementById("center-textbox");
+	stats_textbox = document.getElementById("stats-textbox");
+	stats_textbox2 = document.getElementById("stats-textbox2");
+
+	// canvas stuff
+	canvas = document.getElementById("game-canvas");
+	ctx = canvas.getContext('2d');
+	width = canvas.width;
+	height = canvas.height;
+
+	// click tracking
+	center.addEventListener("mousedown",onClick);
+	center.oncontextmenu = function(){return false;};
+
+	// delta time
+	prev_t_msec = window.performance.now();
+	
+	// music = new Audio("assets/music.mp3");
+	// music.loop = true;
+
+	// parameter initialization
+	//params = paramsGenerate(document.getElementById("params-box"), parameters);
+	params = paramsGenerate(document.body, parameters);
+	//console.log("new params", params);
+	
+	doStart();
+	frame();
+}
+
+function onPlayerReady(event) {
+  //event.target.playVideo();
+}
+function onPlayerStateChange(event) {
+  //console.log("some state change?", event.data);
+}
+function onYouTubeIframeAPIReady() {
+	console.log("youtube state", YT);
+	player = new YT.Player('music-player', {
+		height: '390',
+		width: '640',
+		playerVars: {
+			listType: 'playlist',
+			list: 'PLyQlSCvBJsly9gDjXX2BPkqYf2x37ySu7'
+		},
+		events: {
+			'onReady': onPlayerReady,
+			'onStateChange': onPlayerStateChange
+		}
+	});
+}
+
+function addTarget() {
+	var new_target = {};
+	
+	var spawn_width = params.spawn_width.value;
+	var spawn_height = params.spawn_height.value;
+	var circle_radius_min = params.circle_radius_min.value;
+	var circle_radius_max = params.circle_radius_max.value;
+	
+	// how to randomly generate with respect to the center
+	new_target["x"] = Math.random()*spawn_width + (width - spawn_width)/2;
+	new_target["y"] = Math.random()*spawn_height + (height - spawn_height)/2;
+	new_target["radius"] = Math.random()*(circle_radius_max - circle_radius_min) + circle_radius_min;
+	new_target["fadein"] = 0;
+	
+	//console.log("new target", new_target);
+	targets.push(new_target);
+}
+function updateStats1() {
+	stats_textbox.innerHTML = 
+	 "<p>time : " + time_elapsed_sec.toFixed(1) + "</p>" 
+	 + "<p>hits : " + clicks + "</p>"
+}
+function updateStats2() {
+	//Stats
+	//  total hits
+	//  bullseye hits
+	//  average distance from center
+	//  hits per second
+	//  average latency
+	output = {
+	"hits" : clicks,
+	"seconds elapsed" : time_elapsed_sec.toFixed(1),
+	"hits/sec" : (clicks/time_elapsed_sec).toFixed(2)
+	}
+
+	stats_textbox2.innerHTML = "<p>last stats</p>"
+	for (key in output) {
+		stats_textbox2.innerHTML += "<p>" + key + " : " + output[key] + "</p>";
+	}
+}
+
+// state transitions
+function doStart() {
+	center_textbox.innerHTML="CLICK TO START";
+	state = GameState.START;
+	if (player && player.pauseVideo) player.pauseVideo();
+}
+function doPlay() {
+	center_textbox.style.display = 'block';
+	clicks = 0;
+	time_elapsed_sec = 0;
+	last_click_sec = -10;
+	var last_clicked = 0;
+	state = GameState.PLAY;
+	targets = [];
+	
+	//music.play();
+	
+	if (player && player.playVideo) player.playVideo();
+	
+	
+	for(var i=0; i<params.num_targets.value; i++){
+		addTarget();
+	}
+}
+function doFinish() {
+	center_textbox.style.display = 'block';
+	center.style.cursor = 'auto';
+	center_textbox.innerHTML="CLICK TO RETRY";
+	state = GameState.FINISH;
+	if (player && player.pauseVideo) player.pauseVideo();
+	
+	stats_textbox.innerHTML = "";
+	updateStats2();
+
+}
+
+function onClick(e) {
+	e.preventDefault(); // disables that double click selection thing
+	//console.log("some clickin", e);
+	//console.log(state);
+	if (state == GameState.START || state == GameState.FINISH) {
+		doPlay();
+	}
+	else if (state == GameState.PLAY) {
+	
+		//console.log("mouse", e.offsetX, e.offsetY);
+
+		// scan for circle hits, oldest first
+		for (var i = 0; i < targets.length; i++) {
+			//console.log("target", i, targets[i]['x'], targets[i]['y'], targets[i]['radius']);
+			// something something when the text box captures the mouse clicks we get messed up
+			var rect = center.getBoundingClientRect();
+			offsetY = e.clientY - rect.top;
+			offsetX = e.clientX - rect.left;
+			
+			var hit = 
+				(offsetX - targets[i]['x'])**2 
+				+ (offsetY - targets[i]['y'])**2 
+				<= targets[i]['radius']**2;
+				
+			if (hit) {
+				var hitsound = new Audio('assets/hit.wav');
+				hitsound.play();
+				
+				// remove the element
+				targets.splice(i,1);
+				
+				last_click_sec = time_elapsed_sec;
+				last_click_x = offsetX;
+				last_click_y = offsetY;
+				
+				addTarget();
+				clicks++;
+				updateStats1();
+				return;
+			}
+		}
+	
+		// no hits
+		var misssound = new Audio('assets/miss.wav');
+		misssound.play();
+		doFinish();
+		return;
+	}
+}
+
+function frame() {
+	ctx.clearRect(0, 0, width, height);
+	//console.log("foo");
+
+	// time stuff
+	var t_msec = window.performance.now();
+	var dt_msec = t_msec - prev_t_msec;
+	prev_t_msec = t_msec;
+
+	if (state == GameState.PLAY) {
+		time_elapsed_sec += dt_msec / 1000;
+
+		//ctx.fillStyle = 'rgb(180, 0, 0)';
+		//ctx.fillRect(0, height-10, width, 10);
+		//console.log(time_elapsed_sec);
+
+		if (time_elapsed_sec < 1) {
+			center_textbox.innerHTML = "3";
+		}
+		else if (time_elapsed_sec < 2) {
+			center_textbox.innerHTML = "2";
+		}
+		else if (time_elapsed_sec < 3) {
+			center_textbox.innerHTML = "1";
+		}
+		else {
+			center_textbox.style.display = 'none';
+			center.style.cursor = 'none';
+		}
+		updateStats1();
+
+		// draw circles, in reverse order to copy Osu!
+		// also bless stack overflow
+		for (var i = targets.length - 1; i >= 0; i--) {
+			ctx.beginPath();
+			ctx.arc(targets[i]['x'], targets[i]['y'], targets[i]['radius'], 0, 2 * Math.PI, false);
+			ctx.fillStyle = 'rgba(255, 0, 0, .8)';
+			ctx.fill();
+			ctx.lineWidth = 2;
+			ctx.strokeStyle = '#FFFFFF';
+			ctx.stroke();
+		}
+		
+		// draw sploosh
+		if (time_elapsed_sec - last_click_sec <= .1) {
+			ctx.beginPath();
+			ctx.arc(last_click_x, last_click_y, params.hint_radius.value, 0, 2 * Math.PI, false);
+			ctx.fillStyle = 'white';
+			ctx.fill();
+			ctx.lineWidth = 0;
+			ctx.strokeStyle = '#FFFFFF';
+			ctx.stroke();
+		}
+
+	}
+
+	window.requestAnimationFrame(frame);
+}
---
+++ params.js
@@ -0,0 +1,108 @@
+// generates the parameters and a ui in the parent html elemtn
+// returns an object with parameter values
+// use params.set(pname, value) to set a parameter and update the inputs
+function paramsGenerate(parent, parameters) {
+	params = {};
+	console.log(parameters)
+	for (pname in parameters) {
+		var param = parameters[pname];
+		var paragraph;
+		var input;
+		var slider;
+		paragraph = document.createElement("p");
+		paragraph.innerHTML = "<div style='display:inline-block;width:150px;'>" + pname + "</div>";
+		
+		input = document.createElement("INPUT");
+		input.setAttribute("type", "number");
+		input.setAttribute("min", param.min);
+		input.setAttribute("max", param.max);
+		input.setAttribute("value", param.default);
+		input.setAttribute("style", "width: 40px");
+		
+		slider = document.createElement("INPUT");
+		slider.setAttribute("type", "range");
+		slider.setAttribute("min", param.min);
+		slider.setAttribute("max", param.max);	
+		slider.setAttribute("value", param.default);
+		slider.width = 150;
+		
+		parent.appendChild(paragraph);
+		paragraph.appendChild(input);
+		paragraph.appendChild(slider);
+		
+		let name_closure = pname;
+		// this one forces the value to be in range
+		input.onchange = function () {
+			//console.log("input", name_closure, input.value, this.value);
+			params.Set(name_closure, this.value);
+		};
+		// this one is more active
+		slider.addEventListener('input', function (e) {
+			params.Set(name_closure, e.target.value);
+			//console.log("slide", name_closure, e.target.value);
+		});
+
+		params[pname] = {
+			"paragraph" : paragraph,
+			"slider" : slider,
+			"input" : input,
+			"value" : param.default
+		}
+	}
+	
+	params.Set = function(name, value) {
+		if (!(name in params)) return;
+		config = parameters[name];
+		
+		var new_value = value;
+		if (value > config.max) new_value = config.max;
+		if (value < config.min) new_value = config.min;
+		
+		current = params[name];
+		current.slider.value = new_value;
+		current.input.value = new_value;
+	};
+	
+	return params;
+}
+/* Sample usage
+
+parameters = {
+	'circle_radius_min' : {
+		'min' : 1,
+		'max' : 500,
+		'default' : 50
+	},
+	'circle_radius_max' : {
+		'min' : 1,
+		'max' : 500,
+		'default' : 100
+	},
+	'num_targets' : {
+		'min' : 1,
+		'max' : 40,
+		'default' : 3
+	},
+	'spawn_width' : {
+		'min' : 0,
+		'max' : 800,
+		'default' : 600
+	},
+	'spawn_height' : {
+		'min' : 0,
+		'max' : 600,
+		'default' : 500
+	},
+	'hint_radius' : {
+		'min' : 0,
+		'max' : 500,
+		'default' : 10
+	}
+}
+
+params = paramsGenerate(document.body, parameters);
+params.set('num_targets', 11);
+params.num_targets.value
+
+*/
+
